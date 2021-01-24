4166d09e82e43c5480e73ae2a65740323d6e77a6
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-01-24 20:39:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ var owmAPI = "839e3790699145d87b2fe7fa76e4baaf";
 var currentCity = "";
 var lastCity = "";
 
+var handleErrors = (response) => {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
 
