ed20ab30604b2b4544646fed4710a40fc6666d1f
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-01-24 20:38:49 +0000

    set global variables

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-var apiKey = "839e3790699145d87b2fe7fa76e4baaf"
+var owmAPI = "839e3790699145d87b2fe7fa76e4baaf";
+var currentCity = "";
+var lastCity = "";
 
-$.ajax({
-    method: "GET",
-    url: "https://api.openweathermap.org/data/2.5/weather?q=London&appid=839e3790699145d87b2fe7fa76e4baaf",
-  })
-    .done(function(response) {
-      $( "#results" ).text(JSON.stringify(response));
-    });
 
