2c88b43573a20cbe53389c83eae1357615ec267d
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-01-24 20:41:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ var handleErrors = (response) => {
     return response;
 }
 
+var getCurrentConditions = (event) => {
+
+    let city = $('#search-city').val();
+    currentCity= $('#search-city').val();
+
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + owmAPI;
+    fetch(queryURL)
+    .then(handleErrors)
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+    
+        saveCity(city);
+        $('#search-error').text("");
+
+        let currentWeatherIcon="https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+ 
+        let currentTimeUTC = response.dt;
+        let currentTimeZoneOffset = response.timezone;
+        let currentTimeZoneOffsetHours = currentTimeZoneOffset / 60 / 60;
+        let currentMoment = moment.unix(currentTimeUTC).utc().utcOffset(currentTimeZoneOffsetHours);
